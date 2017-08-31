c72cdc3bc8136ba05ba81a7d102b4a1720b15613
MoeBrowne <oliver.browne@uplyme.com>
2017-08-31 20:45:07 +0100

    Added tests for compressed sitemaps

--- tests/unit/SitemapTest.php
+++ tests/unit/SitemapTest.php
@@ -20,7 +20,7 @@ class SitemapTest extends \Codeception\Test\Unit
     {
     }
 
-    public function testSitemapIsFoundIfStatusCodeIsHttpOk()
+    public function testExistsIsTrueIfSitemapIsFound()
     {
         $responseMethods = [
             'getStatusCode' => 200,
@@ -37,7 +37,7 @@ class SitemapTest extends \Codeception\Test\Unit
         $this->assertTrue($results->exists);
     }
 
-    public function testSitemapIsNotFoundIfStatusCodeIsHttpNotFound()
+    public function testExistsIsFalseIfSitemapIsNotFound()
     {
         $responseMethods = [
             'getStatusCode' => 404,
@@ -52,5 +52,79 @@ class SitemapTest extends \Codeception\Test\Unit
         $results = $test->run('http://example.org');
 
         $this->assertFalse($results->exists);
+    }
+
+    public function testCompressedIsNullIfSitemapIsNotFound()
+    {
+        $responseMethods = [
+            'getStatusCode' => 404,
+            'getResponse' => ''
+        ];
+
+        $responseClass = Stub::make(Response::class, $responseMethods);
+        $client = Stub::make(Client::class, ['request' => $responseClass ]);
+
+        $test = new Sitemap($client);
+
+        $results = $test->run('http://example.org');
+
+        $this->assertFalse($results->exists);
+        $this->assertAttributeEquals(null, 'compressed', $results);
+    }
+
+    public function testCompressedIsTrueIfCompressedSitemapIsFound()
+    {
+        $requestFunc = function ($method, $URL) {
+            if ($URL === 'http://example.org/sitemap.xml') {
+                $responseMethods = [
+                    'getStatusCode' => 200,
+                    'getResponse' => ''
+                ];
+            }
+
+            if ($URL === 'http://example.org/sitemap.xml.gz') {
+                $responseMethods = [
+                    'getStatusCode' => 200,
+                    'getResponse' => ''
+                ];
+            }
+
+            return Stub::make(Response::class, $responseMethods);
+        };
+
+        $client = Stub::make(Client::class, ['request' => $requestFunc ]);
+
+        $test = new Sitemap($client);
+        $results = $test->run('http://example.org');
+
+        $this->assertTrue($results->compressed);
+    }
+
+    public function testCompressedIsFalseIfCompressedSitemapIsNotFound()
+    {
+        $requestFunc = function ($method, $URL) {
+            if ($URL === 'http://example.org/sitemap.xml') {
+                $responseMethods = [
+                    'getStatusCode' => 200,
+                    'getResponse' => ''
+                ];
+            }
+
+            if ($URL === 'http://example.org/sitemap.xml.gz') {
+                $responseMethods = [
+                    'getStatusCode' => 404,
+                    'getResponse' => ''
+                ];
+            }
+
+            return Stub::make(Response::class, $responseMethods);
+        };
+
+        $client = Stub::make(Client::class, ['request' => $requestFunc ]);
+
+        $test = new Sitemap($client);
+        $results = $test->run('http://example.org');
+
+        $this->assertFalse($results->compressed);
     }
 }
