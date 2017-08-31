4ebe5c749091c132d9bd5376a487b3d2e68cf425
MoeBrowne <oliver.browne@uplyme.com>
2017-08-31 20:19:21 +0100

    Added a test to check for if the sitemap is not found

--- tests/unit/SitemapTest.php
+++ tests/unit/SitemapTest.php
@@ -36,4 +36,21 @@ class SitemapTest extends \Codeception\Test\Unit
 
         $this->assertTrue($results->exists);
     }
+
+    public function testSitemapIsNotFoundIfStatusCodeIsHttpNotFound()
+    {
+        $responseData = [
+            'getStatusCode' => 404,
+            'getResponse' => ''
+        ];
+
+        $response = Stub::make(Response::class, $responseData);
+        $request = Stub::make(Client::class, ['request' => $response ]);
+
+        $test = new Sitemap($request);
+
+        $results = $test->run('http://example.org');
+
+        $this->assertFalse($results->exists);
+    }
 }
