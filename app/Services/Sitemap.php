d42b8f042a38d42afa494366676e154c9505b4b3
MoeBrowne <oliver.browne@uplyme.com>
2017-09-01 20:19:10 +0100

    Updated the sitemap test to return an instance of TestResult

--- app/Services/Sitemap.php
+++ app/Services/Sitemap.php
@@ -12,27 +12,28 @@ class Sitemap
         $this->client = $client;
     }
 
-    public function run(string $URL): \stdClass
+    public function run(string $URL): TestResult
     {
-        $results = new \stdClass();
-
         try {
             $res = $this->client->request('GET', $URL . '/sitemap.xml');
         } catch (ClientException $e) {
             $res = $e->getResponse();
         }
 
-        $results->exists = ($res->getStatusCode() === 200);
+        $results = new TestResult();
 
-        if ($results->exists === false) {
-            $results->compressed = null;
-        } else {
+        if ($res->getStatusCode() === 404) {
+            $results->addProblem('A sitemap.xml file could not be found');
+        }
+
+        if ($res->getStatusCode() === 200) {
             try {
                 $res = $this->client->request('GET', $URL . '/sitemap.xml.gz');
             } catch (ClientException $e) {
                 $res = $e->getResponse();
             }
-            $results->compressed = ($res->getStatusCode() === 200);
+
+            $results->addWarning('A compressed sitemap (sitemap.xml.gz) could not be found');
         }
 
         return $results;
