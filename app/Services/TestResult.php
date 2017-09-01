023c11f5cb5c9d751203c1c2cb04ca794df66a61
MoeBrowne <oliver.browne@uplyme.com>
2017-09-01 20:42:21 +0100

    Added a data field to the results

--- app/Services/TestResult.php
+++ app/Services/TestResult.php
@@ -7,6 +7,8 @@ class TestResult
     protected $warnings = [];
     protected $notices = [];
 
+    protected $data = [];
+
     public function addProblem(string $message): void
     {
         array_push($this->problems, $message);
@@ -20,6 +22,11 @@ class TestResult
     public function addNotice(string $message): void
     {
         array_push($this->notices, $message);
+    }
+
+    public function addData(string $key, $value): void
+    {
+        array_push($this->data, [ $key => $value]);
     }
 
     public function getProblems(): array
@@ -37,12 +44,18 @@ class TestResult
         return $this->notices;
     }
 
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
     public function getAllMessages(): array
     {
         return [
             'problems' => $this->getProblems(),
             'warnings' => $this->getWarnings(),
             'notices' => $this->getNotices(),
+            'data' => $this->getData(),
         ];
     }
 
