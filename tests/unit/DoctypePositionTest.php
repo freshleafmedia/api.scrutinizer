5eec3e643709501227c3c43f5095ce6ef87e5273
MoeBrowne <oliver.browne@uplyme.com>
2017-12-03 19:37:14 +0000

    Added a test that checks the test fails when there is no doctype

--- tests/unit/DoctypePositionTest.php
+++ tests/unit/DoctypePositionTest.php
@@ -36,6 +36,22 @@ class DoctypePositionTest extends \Codeception\Test\Unit
         $this->assertTrue(count($results->getProblems()) === 0);
     }
 
+    public function testForFailureIfDoctypeIsMissing()
+    {
+        $responseMethods = [
+            'getBody' => '<html><head>',
+        ];
+
+        $responseClass = Stub::make(Response::class, $responseMethods);
+        $client = Stub::make(Client::class, ['request' => $responseClass ]);
+
+        $test = new DoctypePosition($client);
+
+        $results = $test->run('http://example.org');
+
+        $this->assertFalse(count($results->getProblems()) === 0);
+    }
+
     public function testTheDoctypeChecksAreCaseInsensitive()
     {
         $responseMethods = [
